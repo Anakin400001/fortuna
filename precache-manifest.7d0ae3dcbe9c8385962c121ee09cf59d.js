02214b983192ef9010b0569087b6747d0952d025
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-05-29 11:13:57 +0300

    Updates

---
+++ precache-manifest.7d0ae3dcbe9c8385962c121ee09cf59d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cd1f72b79f1d8266e661ca8ed22dad",
+    "url": "/Anakin400001/fortunaindex.html"
+  },
+  {
+    "revision": "530c65b7bbe24336213e",
+    "url": "/Anakin400001/fortunastatic/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "edb8ceb6aa6f0fe6ba66",
+    "url": "/Anakin400001/fortunastatic/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "530c65b7bbe24336213e",
+    "url": "/Anakin400001/fortunastatic/js/2.1be58915.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/Anakin400001/fortunastatic/js/2.1be58915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb8ceb6aa6f0fe6ba66",
+    "url": "/Anakin400001/fortunastatic/js/main.719c4621.chunk.js"
+  },
+  {
+    "revision": "69b6a659897aa763f7ad",
+    "url": "/Anakin400001/fortunastatic/js/runtime-main.36feb388.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Anakin400001/fortunastatic/media/persik.4e1ec840.png"
+  }
+]);
